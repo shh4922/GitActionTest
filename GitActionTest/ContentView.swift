f552a0cf0d7e5298911c72f3f584443d90d7d983
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2023-10-22 16:14:40 +0900

    Initial Commit

---
+++ GitActionTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitActionTest
+//
+//  Created by Hyeonho on 10/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitActionTest/GitActionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitActionTestApp.swift
+//  GitActionTest
+//
+//  Created by Hyeonho on 10/22/23.
+//
+
+import SwiftUI
+
+@main
+struct GitActionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
